77971c4698282180eb1b2abf5a8b99f57995f257
Jim Neath <jimneath@googlemail.com>
2014-01-15 14:16:18 +0000

    tweaks

--- recipes/cleanup.rb
+++ recipes/cleanup.rb
@@ -6,6 +6,7 @@
 if node[:instance_role][/app|solo/]
   # stop any old puma instances running on port 5000, 5001, etc
   execute 'cleanup old puma workers' do
-    command 'ps aux | egrep "puma.50[0-9]{2}" | xargs kill'
+    command "ps aux | awk '/puma.50[0-9]{2}/{print $2}' | xargs kill"
+    only_if "ps aux | egrep puma.50[0-9]{2}"
   end
 end
